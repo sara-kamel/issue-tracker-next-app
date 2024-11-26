a8c6b48b9a924a7735c1cd442b3f79901b6e60d7
Sara Kamel <sarakamel591@gmail.com>
2024-11-26 08:22:37 -0800

    Build NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { FaBug } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
+
+const NavBar = () => {
+  const navLinks = [
+    { href: '/', label: 'Dashboard' },
+    { href: '/issue', label: 'Issues' }
+  ]
+  const currentPath = usePathname()
+  return (
+    <nav className='flex space-x-6 p-4 mb-3 border-b-2 h-14 items-center'>
+      <Link href='/'>
+        <FaBug />
+      </Link>
+      {navLinks.map(link => (
+        <ul className='flex'>
+          <li
+            className={`${
+              link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
+            } hover:text-zinc-800 `}
+          >
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
+          </li>
+        </ul>
+      ))}
+    </nav>
+  )
+}
+
+export default NavBar
