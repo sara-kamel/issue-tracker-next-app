b02ae6502079cbccec768e12a8df0444e75959e1
Sara Kamel <sarakamel591@gmail.com>
2025-02-18 09:22:47 -0800

    Add Skeleton to Login

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Skeleton } from './components'
 import React from 'react'
 import Link from 'next/link'
 import { FaBug } from 'react-icons/fa'
@@ -60,7 +61,7 @@ const NavLinks = () => {
 }
 const AuthStatus = () => {
   const { status, data: session } = useSession()
-  if (status === 'loading') return null
+  if (status === 'loading') return <Skeleton width='3rem' />
   if (status === 'unauthenticated')
     return (
       <Link className='nav-link ' href='/api/auth/signin'>
