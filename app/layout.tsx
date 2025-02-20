d4980cc5fd44ea706425501c50d697ad0f0fb2a8
Sara Kamel <sarakamel591@gmail.com>
2025-02-20 06:36:35 -0800

    Set up React query

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
 import { Container, Theme } from '@radix-ui/themes'
 import AuthProvider from './auth/Provider'
+import QueryClientProvider from './QueryClientProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,14 +22,16 @@ export default function RootLayout ({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Theme accentColor='plum' grayColor='slate'>
-          <AuthProvider>
-            <NavBar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </AuthProvider>
-        </Theme>
+        <QueryClientProvider>
+          <Theme accentColor='plum' grayColor='slate'>
+            <AuthProvider>
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </AuthProvider>
+          </Theme>
+        </QueryClientProvider>
       </body>
     </html>
   )
