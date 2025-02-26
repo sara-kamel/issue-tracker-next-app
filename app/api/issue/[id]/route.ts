1bfab115eb7470bd96738c58bb386c9938f053ce
Sara Kamel <sarakamel591@gmail.com>
2025-02-26 06:12:21 -0800

    Enhance the API to assign issues

--- app/api/issue/[id]/route.ts
+++ app/api/issue/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from '@/app/auth/authOptions'
-import { issueSchema } from '@/app/validationSchema'
+import { PatchIssueSchema } from '@/app/validationSchema'
 import prisma from '@/prisma/client'
 import { getServerSession } from 'next-auth'
 import { NextRequest, NextResponse } from 'next/server'
@@ -8,12 +8,22 @@ export async function PATCH (
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions)
-  if (!session) return NextResponse.json({}, { status: 401 })
+  // const session = await getServerSession(authOptions)
+  // if (!session) return NextResponse.json({}, { status: 401 })
+
   const body = await request.json()
-  const validation = issueSchema.safeParse(body)
+  const validation = PatchIssueSchema.safeParse(body)
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 })
+  }
+  const { assignedToUserId, title, description } = body
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId }
+    })
+    if (!user) {
+      return NextResponse.json({ error: ' Invalid User' }, { status: 400 })
+    }
   }
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
@@ -23,10 +33,15 @@ export async function PATCH (
   }
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: { title: body.title, description: body.description }
+    data: {
+      title,
+      description,
+      assignedToUserId
+    }
   })
   return NextResponse.json(updatedIssue)
 }
+
 export async function DELETE (
   request: NextRequest,
   { params }: { params: { id: string } }
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { optional, z } from 'zod'
 
 export const issueSchema = z.object({
   title: z
@@ -10,4 +10,25 @@ export const issueSchema = z.object({
     .string()
     .min(3, 'Description is too short')
     .nonempty('Title is required')
+})
+
+export const PatchIssueSchema = z.object({
+  title: z
+    .string()
+    .nonempty('Title is required')
+    .min(3, 'Title is too short')
+    .max(255, 'too long')
+    .optional(),
+  description: z
+    .string()
+    .min(3, 'Description is too short')
+    .max(65535)
+    .nonempty('Title is required')
+    .optional(),
+    assignedToUserId: z
+    .string()
+    .min(1, 'AssignedToUserId is required.')
+    .max(255)
+    .optional()
+    .nullable()
 })
