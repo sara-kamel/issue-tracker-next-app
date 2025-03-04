1e621fa099ebac47b1ef20a9beed312cb864b0de
Sara Kamel <sarakamel591@gmail.com>
2025-03-04 10:11:02 -0800

    Show toast notifications

--- app/issue/[id]/AssigneeSelect.tsx
+++ app/issue/[id]/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { Skeleton } from '../../components'
+import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -21,30 +22,49 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (error) return null
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || 'no user'}
-      onValueChange={userId => {
-        console.log('Updating issue:', { id: issue.id, userId })
-        if (userId === 'no user') {
-          axios.patch(`/api/issue/${issue.id}`, { assignedToUserId: null })
-        } else {
-          axios.patch(`/api/issue/${issue.id}`, { assignedToUserId: userId })
-        }
-      }}
-    >
-      <Select.Trigger placeholder='Assign...' />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value={'no user'}>Unassigned</Select.Item>
-          {users?.map(user => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || 'no user'}
+        // onValueChange={async userId => {
+        //   try {
+        //     if (userId === 'no user') {
+        //       await axios.patch(`/xapi/issue/${issue.id}`, {
+        //         assignedToUserId: null
+        //       })
+        //     } else {
+        //       await axios.patch(`/xapi/issue/${issue.id}`, {
+        //         assignedToUserId: userId
+        //       })
+        //     }
+        //   } catch (error) {
+        //     toast.error('Changes Could not be saved')
+        //   }
+        // }}
+        onValueChange={async userId => {
+          try {
+            await axios.patch(`/api/issue/${issue.id}`, {
+              assignedToUserId: userId === 'no user' ? null : userId
+            })
+          } catch (error) {
+            toast.error('Changes could not be saved')
+          }
+        }}
+      >
+        <Select.Trigger placeholder='Assign...' />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value={'no user'}>Unassigned</Select.Item>
+            {users?.map(user => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   )
 }
 
